6c1ff894e05888f8dcb2421ff6dc6f23cca1f6f7
OrabutC <orabutc64@nu.ac.th>
2023-09-27 16:04:26 +0700

    finish ch8

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,24 +1,28 @@
 module.exports = {
-// get all user
-    index(req, res) {
-        res.send('ดูข้อมูลผู้ใช้ทั้งหมด')
-       },
 
-// create user
-       create(req, res) {
-        res.send('สร้างผู้ใช้งาน' + JSON.stringify(req.body))
-       },
-// edit user, suspend, active
-       put(req, res) {
-        res.send('แก้ไขข้อมูลผู้ใช้' + req.params.userId + " : " +JSON.stringify(req.body.name))
-       },
-// delete user
-       delete(req, res) {
-    res.send('ลบข้อมูลผุ้ใช้' + req.params.userId + " : " + JSON.stringify(req.body.name))
+    // Show user by id
+    show(req, res) {
+        res.send('ดูข้อมูลผู้ใช้งาน ' + req.params.userId + ' : ' + JSON.stringify(req.body));
     },
-// get user by id
-     show (req, res) {
-       res.send('ดูข้อมูลผู้ใช้งาน: ' + req.params.userId + " : " + JSON.stringify(req.body))
-    }
-   }
 
+    // Get all users
+    index(req, res) {
+        res.send('ดูข้อมูลผู้ใช้งาน');
+    },
+
+    // Create user
+    create(req, res) {
+        res.send('ทำการสร้างผู้ใช้งาน' + JSON.stringify(req.body));
+    },
+
+    // Edit user
+    put(req, res) {
+        res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + ' : ' + JSON.stringify(req.body.name));
+    },
+
+    // Delete user
+    delete(req, res) {
+        res.send('ทำการลบผู้ใช้งาน:' + req.params.userId + ' : ' + JSON.stringify(req.body));
+    }
+
+};
